51c3fbaa4d9ed48bc9ef01d858f853f7a582d751
Sebastian Lopez Wells <106717053+SebastianLopezWells@users.noreply.github.com>
2022-08-16 10:19:21 -0700

    Class 3

--- data.py
+++ data.py
@@ -5,3 +5,46 @@ me = {
     "hobbies": [],
     "address": {"number": 147, "street": "Vets", "city": "Tijuana"},
 }
+
+catalog = [
+    {
+        "_id": "1",
+        "title": "Apple",
+        "price": 13.05,
+        "category": "Fruit",
+        "image": "Apple.jpg",
+        "max": 10,
+    },
+    {
+        "_id": "2",
+        "title": "Switch",
+        "price": 345.05,
+        "category": "Console",
+        "image": "switch.jpg",
+        "max": 10,
+    },
+    {
+        "_id": "3",
+        "title": "Coke",
+        "price": 7.05,
+        "category": "Drinks",
+        "image": "coke.jpg",
+        "max": 10,
+    },
+    {
+        "_id": "4",
+        "title": "Grapes",
+        "price": 4.05,
+        "category": "Fruit",
+        "image": "Grape.jpg",
+        "max": 10,
+    },
+    {
+        "_id": "5",
+        "title": "Xbox Series X",
+        "price": 485.05,
+        "category": "Console",
+        "image": "XboxSX.jpg",
+        "max": 10,
+    },
+]
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json
 from data import me
+from data import catalog
 
 app = Flask(__name__)
 
@@ -37,4 +38,50 @@ def about_api():
     return json.dumps(me)
 
 
-app.run(debug=True)
+@app.get("/api/catalog")
+def get_catalog():
+    # return the list of products
+    return json.dumps(catalog)
+
+
+@app.get("/api/product/<id>")
+def get_product_by_id(id):
+    for x in catalog:
+        if x["_id"] == id:
+            return json.dumps(x)
+    return json.dumps("ERROR: ID is not valid")
+
+
+@app.get("/api/products/<category>")
+def get_product_by_category(category):
+    result = []
+    for x in catalog:
+        if x["category"].lower() == category.lower():
+            result.append(x)
+    return json.dumps(result)
+
+
+@app.get("/api/count")
+def cat_count():
+    count = len(catalog)
+    return json.dumps(count)
+
+
+@app.get("/api/catalog/total")
+def cat_price():
+    total = 0
+    for x in catalog:
+        total += x["price"]
+    return json.dumps(total)
+
+
+@app.get("/api/catalog/cheapest")
+def cat_cheapest():
+    cheapest = catalog[0]
+    for x in catalog:
+        if x["price"] < cheapest["price"]:
+            cheapest = x
+    return json.dumps(cheapest)
+
+
+# app.run(debug=True)
